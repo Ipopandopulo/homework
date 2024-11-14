0520e571d17e392c95f1af21514d65ff047b5d9c
Ipopandopulo <148614488+Ipopandopulo@users.noreply.github.com>
2024-11-14 10:08:14 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,27 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    string = string.upper()
+    for i in range(len(list_to_search)):
+        list_to_search[i] = list_to_search[i].upper()
+
+    return string in list_to_search
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
